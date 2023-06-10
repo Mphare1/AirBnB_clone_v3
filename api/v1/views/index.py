90379770e866dca7a91f789c8ad8b00542f5d868
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-06-10 22:05:59 +0000

    Get som stats from database

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,35 @@
 """Check the status of api"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.user import User
+from models.state import State
+from models.review import Review
 
+
+objects = {
+    Amenity: "amenities",
+    City: "cities",
+    Place: "places",
+    User: "users",
+    State: "states",
+    Review: "reviews"
+    }
 
 @app_views.route('/status')
 def status():
     """status function"""
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def stats():
+    """return the stats of objects"""
+    my_stats = { }
+
+    for k, v in objects.items():
+        my_stats[v] = storage.count(k)
+
+    return jsonify(my_stats)
