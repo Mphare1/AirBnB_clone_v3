a2577987a0683dc3ca7a5c320b07bdcae47c9e72
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-06-10 21:39:34 +0000

    Documented views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Check the status of api"""
 from api.v1.views import app_views
 from flask import jsonify
 
 
 @app_views.route('/status')
 def status():
+    """status function"""
     return jsonify({'status': 'OK'})
