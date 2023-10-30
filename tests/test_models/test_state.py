6d3fcc5ceb5dac04950b1f33cb5efe4293ec8c29
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-10-30 21:11:28 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,7 +2,6 @@
 """
 Contains the TestStateDocs classes
 """
-
 from datetime import datetime
 import inspect
 import models
@@ -11,7 +10,6 @@ from models.base_model import BaseModel
 import pep8
 import unittest
 State = state.State
-
 
 class TestStateDocs(unittest.TestCase):
     """Tests to check the documentation and style of State class"""
@@ -56,7 +54,6 @@ class TestStateDocs(unittest.TestCase):
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
-
 class TestState(unittest.TestCase):
     """Test the State class"""
     def test_is_subclass(self):
@@ -83,7 +80,7 @@ class TestState(unittest.TestCase):
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
         for attr in s.__dict__:
-            if attr is not "_sa_instance_state":
+            if attr != "_sa_instance_state":
                 self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
